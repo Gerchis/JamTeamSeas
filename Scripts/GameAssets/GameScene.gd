5bc8197ff84c414ba069cb56ca691eb8ce624120
Gerchis <56065317+Gerchis@users.noreply.github.com>
2021-11-17 14:17:15 +0100

    Particles

--- Scripts/GameAssets/GameScene.gd
+++ Scripts/GameAssets/GameScene.gd
@@ -3,14 +3,12 @@ extends Node2D
 export var wb_shader : ShaderMaterial
 export(Array, NodePath) var sprite_list : Array
 
-func _ready() -> void:
-	grayscale_assets()
 
-func grayscale_assets():
+func grayscale_assets(_factor : float):
 	for sprite_path in sprite_list:
 		
 		var sprite = get_node(sprite_path)
 		
 		if sprite.get_material() == wb_shader:
-			sprite.material.set_shader_param("factor",0)
+			sprite.material.set_shader_param("factor",_factor)
 
