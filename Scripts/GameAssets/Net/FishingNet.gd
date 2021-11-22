9f22016ac61bb029eeb62bccc439622a3bb422e7
Ethirn <xedurami@gmail.com>
2021-11-22 11:30:14 +0100

    f

--- Scripts/GameAssets/Net/FishingNet.gd
+++ Scripts/GameAssets/Net/FishingNet.gd
@@ -1,18 +1,27 @@
 extends Node2D
 
 export(NodePath) var run_anim
+export(NodePath) var player
 
 onready var anim := $AnimationPlayer
 onready var trigger := $NetTrigger
 
+var passed := false
+
 signal stop_movement
 
 func _on_NetTrigger_body_shape_entered(body_id: int, body: Node, body_shape: int, local_shape: int) -> void:
-	anim.set_current_animation("Pre_Net")
 	
-	emit_signal("stop_movement")
-	
-	trigger.queue_free()
+	if $NetTrigger.global_position.x < get_node(player).global_position.x:
+		if passed:
+			anim.set_current_animation("Pre_Net")
+			
+			emit_signal("stop_movement")
+			
+			trigger.queue_free()
+		else:
+			passed = true
+
 
 
 func _on_PLayer_start_running() -> void:
