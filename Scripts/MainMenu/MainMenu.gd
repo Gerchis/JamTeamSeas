b5929ff7b8b21d0198126d074a27db3c11a1de9c
Gerchis <56065317+Gerchis@users.noreply.github.com>
2021-11-23 11:39:48 +0100

    mute_function

--- Scripts/MainMenu/MainMenu.gd
+++ Scripts/MainMenu/MainMenu.gd
@@ -4,3 +4,9 @@ export (String, FILE) var game_scene
 
 func _on_Button_pressed() -> void:
 	SceneManager.goto_scene(game_scene)
+
+func mute_audio():
+	if AudioServer.get_bus_volume_db(0) >= 0:
+		AudioServer.set_bus_volume_db(0, -80)
+	else:
+		AudioServer.set_bus_volume_db(0,0)
