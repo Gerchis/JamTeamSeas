122444821e9abcb9f38e2958360b92f537a2aab5
Gerchis <56065317+Gerchis@users.noreply.github.com>
2021-11-23 12:33:22 +0100

    mute volumen

--- Scripts/MainMenu/MainMenu.gd
+++ Scripts/MainMenu/MainMenu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 export (String, FILE) var game_scene
+export (Array, Texture) var audio_texture
 
 func _on_Button_pressed() -> void:
 	SceneManager.goto_scene(game_scene)
@@ -8,5 +9,11 @@ func _on_Button_pressed() -> void:
 func mute_audio():
 	if AudioServer.get_bus_volume_db(0) >= 0:
 		AudioServer.set_bus_volume_db(0, -80)
+		$MuteButton.texture = audio_texture[0]
 	else:
 		AudioServer.set_bus_volume_db(0,0)
+		$MuteButton.texture = audio_texture[1]
+
+
+func _on_Button2_button_down() -> void:
+	mute_audio()
